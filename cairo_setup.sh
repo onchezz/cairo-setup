e7e2e523056d088b82101f45b8744be7b3b0fe3b
Onchez <56371447+onchezz@users.noreply.github.com>
2023-08-30 13:19:49 +0300

    Update cairo_setup.sh

--- cairo_setup.sh
+++ cairo_setup.sh
@@ -1,38 +1,10 @@
 #!/bin/bash
 
-# Check if Rust is installed
-if ! [ -x "$(command -v rustc)" ]; then
-
-  # Rust is not installed, proceed with install
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-  source $HOME/.cargo/env
-  
-  
-else
-  echo "Rust is already installed!"
-
-  rustup override set stable && rustup update
-fi
-
-# Check if cairo is installed
-if ! [ -x "$(command -v cairo-run)" ]; then
-
-  # cairo is not installed, proceed with install
- curl -L https://github.com/franalgaba/cairo-installer/raw/main/bin/cairo-installer | bash
-  
-  
-else
-  echo "Cairo is already installed!"
-
-  rustup override set stable && rustup update
-fi
-# install cairo 
-# curl -L https://github.com/franalgaba/cairo-installer/raw/main/bin/cairo-installer | bash
-
 
 
 #install scarb
 # Check if the terminal is Bash
+function setup_cairo(){
 if [[ "$SHELL" =~ "bash" ]]; then
 
   
@@ -47,7 +19,8 @@ if [[ "$SHELL" =~ "bash" ]]; then
   echo 'command -v cairo-compile >/dev/null || export PATH="$CAIRO_ROOT/target/release:$PATH"' >> ~/.bash_profile
 
 
-  echo " cairo is succefully and set up  "  installed 
+  echo " cairo is succefully configured   "  
+  check_scarb 
 
 elif  [[ "$SHELL" =~ "zsh" ]]; then
 
@@ -58,7 +31,8 @@ elif  [[ "$SHELL" =~ "zsh" ]]; then
   
   echo 'export CAIRO_ROOT="$HOME/.cairo"' >> ~/.zsh_profile
   echo 'command -v cairo-compile >/dev/null || export PATH="$CAIRO_ROOT/target/release:$PATH"' >> ~/.zsh_profile
-  check_scarb
+   echo " cairo is succefully configured   " 
+  # check_scarb
 
 
 else
@@ -67,46 +41,35 @@ else
   echo "The terminal is not Zsh neither bash "
 
 fi
+# check_scarb
+}
+
+
+#cheking if scarb is installed 
+
+
 function install_scarb(){
+
 read -p "would you like to install scarb: ?? (  y/n ) " scarb
 echo $scarb
 
 if [[ "$scarb" = "y" || "yes" ]]; then 
-
+  echo "installing scarb..... "
 #installing scarb 
   curl --proto '=https' --tlsv1.2 -sSf https://docs.swmansion.com/scarb/install.sh | sh
+  check_forge
 else
-  check_katana
+ 
+
+ check_forge
+ 
 fi
+
+
 }
-function check_scarb(){
-if command -v scarb >/dev/null; then
 
-  # The command "scarb " exists, scarb is intalled
-  echo "scarb tool is already installed "
-  
 
-else
 
-  # The command "ls" does not exist, so print a message to the user
-  echo "installing scarb ";
-  install_scarb
-  
-
-fi
-}
-function install_forge(){
-read -p "would you like to install starknet-foundry : ?? (  y/n ) " forge
-echo $forge
-
-if [[ "$forge" = "y" || "yes" ]]; then 
-
-#installing Starknet foundry 
-  curl -L https://raw.githubusercontent.com/foundry-rs/starknet-foundry/master/scripts/install.sh | sh
-  
-  
-fi
-}
 function install_katana(){
 read -p "would you like to install katana local development node??  (y/n)  " katana
 if [[ "$katana" = "y" || "yes" ]]; then 
@@ -119,17 +82,81 @@ if [[ "$katana" = "y" || "yes" ]]; then
 
 #check installation
   echo Katna installed succefully 
-  install_forge
-else
-  install_forge
+  
+elif [[ "$katana" = "n" || "no" ]]; then 
+ check_forge
+
+  # install_forge
+  exit
+else 
+  exit
 fi
+# install_forge
+}
+
+function install_forge(){
+read -p "would you like to install starknet-foundry : ?? (  y/n ) " forge
+echo $forge
+
+if [[ "$forge" = "y" || "yes" ]]; then 
+
+#installing Starknet foundry 
+  curl -L https://raw.githubusercontent.com/foundry-rs/starknet-foundry/master/scripts/install.sh | sh
+  
+else
+ exit
+fi
+}
+
+function check_forge(){
+if command -v snforge >/dev/null; then
+
+  # The command "ls" exists, so print a message to the user
+  echo "starknet forge  development environment is installed "
+  
+
+else
+
+  # The command "ls" does not exist, so print a message to the user
+  echo "installing snforge... "
+  install_forge
+
+
+fi
+echo " exiting script"
+}
+
+
+
+
+function check_scarb(){
+  #cheking if scarb is configured
+  echo "cheking scarb ..."
+if ! [ -x "$(command -v "scarb --version")" ]; then
+# if command -v scarb >/dev/null; then
+echo "scarb installed and configured";
+  check_katana
+  
+
+else
+
+  
+  echo "scarb tool not installed "
+  install_scarb
+  check_katana
+  
+  
+
+fi
+
 }
 
 function check_katana(){
 if command -v katana >/dev/null; then
 
   # The command "ls" exists, so print a message to the user
-  echo "Katana local development node is installed "
+  echo "Katana local development  is installed "
+  check_forge
   
 
 else
@@ -141,3 +168,54 @@ else
 
 fi
 }
+
+
+# Check if cairo is installed
+function check_cairo(){
+if ! [ -x "$(command -v "cairo-run --version")" ]; then
+
+  # cairo is not installed, proceed with install
+  echo "Cairo is already installed!"
+  check_scarb
+  exit
+  
+else
+    # rustup override set stable && rustup update
+  rustup override set stable && rustup update
+  curl -L https://github.com/franalgaba/cairo-installer/raw/main/bin/cairo-installer | bash
+  echo "configuring cairo"
+  setup_cairo
+
+  
+
+
+fi
+# check_scarb
+}
+
+#checks 
+function check_rust_installation(){
+#check rust installation
+if ! [ -x "$(command -v rustc)" ]; then
+
+  # Rust is not installed, proceed with install
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source $HOME/.cargo/env
+  exit
+  
+else
+  echo "Rust is already installed!"
+  check_cairo
+  exit
+
+  # rustup override set stable && rustup update
+fi
+# check_cairo
+}
+# install cairo 
+# curl -L https://github.com/franalgaba/cairo-installer/raw/main/bin/cairo-installer | bash
+
+
+
+
+check_rust_installation
